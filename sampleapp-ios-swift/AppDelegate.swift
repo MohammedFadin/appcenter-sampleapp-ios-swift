dbae659d4ba66105a230e712585d681aaa192799
Mohammed Fadin <mfadin@sa.ibm.com>
2021-12-14 15:25:25 +0400

    update appDelegate

--- sampleapp-ios-swift/AppDelegate.swift
+++ sampleapp-ios-swift/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Enabled App Center SDK verbose logs
         AppCenter.logLevel = .verbose
         
-        AppCenter.start(withAppSecret: "<APP SECRET HERE>", services: [
+        AppCenter.start(withAppSecret: "dec34ac1-8707-41da-800c-eaec533b56b6", services: [
             Analytics.self,
             Crashes.self,
         ])
